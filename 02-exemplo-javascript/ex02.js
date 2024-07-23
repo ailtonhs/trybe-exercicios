82d87007696511574d2a5ec37a284aa8f495f85c
ailtonhs <ailtonhenrick@outlook.com>
2024-07-23 20:50:20 -0300

    Declaração de variáveis e estrutura condicional

---
+++ 02-exemplo-javascript/ex02.js
@@ -0,0 +1,18 @@
+let num1 = 10;
+let num2 = 10;
+let word = "Palavra";
+let xablau = null;
+let babalu = undefined;
+
+if (num1 > num2) {
+    console.log("Num1 é o maior");
+} else if (num1 == num2) {
+    console.log("Empate");
+} else {
+    console.log("Num2 é o maior");
+}
+
+console.log(1 === '1');
+
+
+
