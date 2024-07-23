9ba01ffa6ab481c3e292663c90bb116d6fc6f4de
ailtonhs <ailtonhenrick@outlook.com>
2024-07-23 20:57:21 -0300

    Cria algumas variáveis constante e imprimir

---
+++ 02-exemplo-javascript/ex03.js
@@ -0,0 +1,19 @@
+// Exercício 1
+
+// Neste exercício vamos criar algumas variáveis constantes e imprimir as variáveis criadas.
+
+// Questão 1. Crie uma constante chamada myName e atribua a ela o seu nome (Exemplo: Carolina);
+
+// Questão 2. Crie uma constante chamada birthCity e atribua a ela a sua cidade natal;
+
+// Questão 3. Crie uma constante chamada birthYear e atribua a ela o ano em que você nasceu;
+
+// Questão 4. Utilize o console.log() para imprimir as variáveis que você criou.
+
+const myName = "Ailton";
+const birthCity = "Vicência";
+const birthYear = 32;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
