1d0ec61989b7ef8bfdec2fc911541a34debfc1e0
ailtonhs <ailtonhenrick@outlook.com>
2024-07-25 15:41:27 -0300

    Funções JavaScript

---
+++ 04-funcoes/funcoes.js
@@ -0,0 +1,20 @@
+//Exemplo 1
+
+
+function sum(num1, num2) {
+ return num1 + num2
+};
+
+console.log(sum( 10, 5));
+console.log(sum( 100, 75));
+
+//Exemplo 2
+
+const peoplename = 'Hulk';
+
+const helloPeople = (name) => {
+    return `Olá ${name}, tudo bem?`;
+}
+
+console.log(helloPeople('Ahs'));
+console.log(helloPeople('Bernardo'));
