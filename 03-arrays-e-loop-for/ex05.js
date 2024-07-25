5c443393be72a26ce68d9d16eda2bb9deb0dc656
ailtonhs <ailtonhenrick@outlook.com>
2024-07-25 10:43:06 -0300

    Sorteio da mega-sena

--- 03-arrays-e-loop-for/ex05.js
+++ 03-arrays-e-loop-for/ex05.js
@@ -1,10 +1,29 @@
 const lotteryNumbers = [];
-const fistGame = [4, 8, 15, 16, 23, 42];
+const firstGame = [4, 8, 15, 16, 23, 42];
 
-
+//sorteio dos números
 for (let index = 0; index < 6; index += 1) {
     lotteryNumbers.push(Math.ceil(Math.random() * 60));
 };
 
-console.log(lotteryNumbers);
+// Comparação do sorteio com o meu jogo
+
+let numberOfHits= 0;
+let = hits = [];
+
+for (let indexLottery = 0; indexLottery < lotteryNumbers.length; indexLottery += 1) {
+    for(indexGame = 0; indexGame < firstGame.length; indexGame += 1) {
+        if (lotteryNumbers[indexLottery] === firstGame[indexGame]) {
+            numberOfHits += 1;
+            hits.push(lotteryNumbers[indexLottery]);
+        }
+    }
+}
+
+//Quantidade de acertos
+
+console.log(`Números sorteados: ${lotteryNumbers}`);
+console.log(`O Jogo: ${firstGame}`);
+console.log(`Quantidade de acertos: ${numberOfHits}`);
+console.log(`Números acertados: ${hits}`);
 
