2534e1decabe811239f5b4dbb68e5735e456dc1f
ailtonhs <ailtonhenrick@outlook.com>
2024-12-10 13:59:03 -0300

    Fixando os aprendizados

---
+++ 02-exemplos-javascript/02-fixando-aprendizado/ex001/script.js
@@ -0,0 +1,9 @@
+const myName = "Ailton";
+const birthCity = "Vicência";
+const birthYear = 1992;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+
---
+++ 02-exemplos-javascript/02-fixando-aprendizado/ex005/script.js
@@ -0,0 +1,21 @@
+/*
+const personAge = 17;
+
+let canVote;
+
+
+if (personAge >= 16) {
+    canVote = "Pode votar!";
+} else {
+    canVote = "Não pode votar!";
+}
+
+console.log(canVote);
+*/
+
+const personAge = 25;
+
+let canVote = personAge >= 16 ? "Pode votar!" : "Não pode votar!";
+
+console.log(canVote);
+
