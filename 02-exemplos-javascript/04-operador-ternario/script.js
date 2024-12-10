c722304af9a7ac5888ef0e8d7d54693b772a3519
ailtonhs <ailtonhenrick@outlook.com>
2024-12-10 14:01:15 -0300

    Operador ternário

---
+++ 02-exemplos-javascript/04-operador-ternario/script.js
@@ -0,0 +1,9 @@
+// A sintaaxe básica do eperador ternário é;
+//"Condição" ? "retorno se verdadeira" : "Retorno se falsa";
+
+let temperature = 30;
+let weather = temperature > 30 ? "Tá quente" : "Tá de boa";
+
+//let weather = temperature > 30 && "Tá quente" //apenas se for verdadeira
+
+console.log(` A temperatura é ${temperature}, portando, ${weather}`);
