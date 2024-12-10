e04f30f272f4ea2ee30c7d307af107dfe3b3701f
ailtonhs <ailtonhenrick@outlook.com>
2024-12-10 13:56:49 -0300

    Declaração de variáveis e estrutura condicional

---
+++ 02-exemplos-javascript/01-declaracao-de-variaveis-e-estrutura-condicional/script.js
@@ -0,0 +1,24 @@
+let num1 = 100; //number
+let num2 = 10;
+let word = "Palavra"; //string
+let xablau = null; //nulo
+let babalu = undefined; // undefined
+
+//num1 = 900; //reatribuição num1
+//console.log(num1);
+
+if (num1 > num2) {
+    console.log("Num1 é o maior");
+    
+} else if (num1 === num2) {
+    console.log("Empate");
+    
+} else {
+    console.log("Num2 é o maior");
+    
+};
+
+console.log(1 === "1");//false
+console.log(1 == "1");//true
+
+
